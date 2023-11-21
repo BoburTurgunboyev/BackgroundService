30cdf5b8a14ebcef28221804707008eed83743b7
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-21 15:44:57 +0500

    service

--- BackGroundSevices/Program.cs
+++ BackGroundSevices/Program.cs
@@ -1,25 +1,15 @@
+using BackGroundSevices;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<HostOptions>(options =>
+{
+    
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<ExampleService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
