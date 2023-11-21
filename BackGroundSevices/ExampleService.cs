44169dd4118620fbd5d5ec021b3a5589edcf3b2d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-21 15:45:03 +0500

    service

---
+++ BackGroundSevices/ExampleService.cs
@@ -0,0 +1,23 @@
+﻿namespace BackGroundSevices
+{
+    public class ExampleService : IHostedService
+    {
+        private readonly ILogger<ExampleService> _logger;
+
+        public ExampleService(ILogger<ExampleService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async  Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("StartSaync");
+            await Task.Delay(20000);
+        }
+
+        public async  Task StopAsync(CancellationToken cancellationToken)
+        {
+           _logger.LogInformation("StopAsync");
+        }
+    }
+}
