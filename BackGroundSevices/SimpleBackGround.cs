a83b7682786060f2c90902d08792cadcf861f795
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-21 15:44:51 +0500

    service

---
+++ BackGroundSevices/SimpleBackGround.cs
@@ -0,0 +1,14 @@
+﻿namespace BackGroundSevices
+{
+    public class SimpleBackGround : BackgroundService
+    {
+        protected async  override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //while(!stoppingToken.IsCancellationRequested)
+            //{
+                Console.WriteLine("Irakl");
+                //await Task.Delay(1000);
+            //}
+        }
+    }
+}
